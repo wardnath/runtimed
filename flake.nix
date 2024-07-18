5c3a52e439a00023dc023fb5a3b0124b58beb79b
Nathan <wardnath.alt@gmail.com>
2024-07-18 10:31:03 -0400

    kernel works

--- flake.nix
+++ flake.nix
@@ -27,25 +27,21 @@
           jupyter
         ];
 
-        createKernel = pkgs: kernelName: language: 
-          pkgs.writeTextFile {
-            name = "${kernelName}-kernel.json";
-            text = builtins.toJSON {
-              argv = ["${pkgs.python3}/bin/python" "-m" "ipykernel_launcher" "-f" "{connection_file}"];
-              display_name = kernelName;
-              language = language;
-            };
-            destination = "/share/jupyter/kernels/${kernelName}/kernel.json";
-          };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          ipykernel
+          numpy
+          pandas
+          matplotlib
+          scipy
+        ]);
 
-        kernelPackage = pkgs.symlinkJoin {
-          name = "custom-jupyter-kernel";
-          paths = [
-            (createKernel pkgs "CustomPython" "python")
-            pkgs.python3
-            pkgs.python3Packages.ipykernel
-          ];
-        };
+        kernelspec = pkgs.runCommand "jupyter-kernel" {} ''
+          export HOME=$out
+          mkdir -p $out/share/jupyter/kernels/my-data-science-kernel
+          ${pythonEnv}/bin/python -m ipykernel install --prefix=$out --name="my-data-science-kernel"
+          sed -i 's|"python3"|"${pythonEnv}/bin/python"|' $out/share/jupyter/kernels/my-data-science-kernel/kernel.json
+          cp $out/share/jupyter/kernels/my-data-science-kernel/kernel.json $out/kernelspec.json
+        '';
 
         buildRustPackage = { pname, cargoToml }: pkgs.rustPlatform.buildRustPackage {
           inherit pname;
@@ -57,9 +53,10 @@
           };
 
           nativeBuildInputs = commonInputs;
-          buildInputs = [ pkgs.openssl pkgs.jupyter kernelPackage ];
+          buildInputs = [ pkgs.openssl pkgs.jupyter kernelspec ];
 
           buildPhase = ''
+            export KERNELSPEC_PATH=$out/kernelspec.json
             export OPENSSL_DIR=${pkgs.openssl.dev}
             export OPENSSL_INCLUDE_DIR=${pkgs.openssl.dev}/include
             export OPENSSL_LIB_DIR=${pkgs.openssl.out}/lib
@@ -70,8 +67,9 @@
           installPhase = ''
             mkdir -p $out/bin
             cp target/release/${pname} $out/bin/
-            mkdir -p $out/share
-            cp -r ${kernelPackage}/share $out/
+            mkdir -p $out/share/jupyter
+            cp -r ${kernelspec}/share/jupyter/kernels $out/share/jupyter/
+            cp ${kernelspec}/kernelspec.json $out/
           '';
 
           doCheck = false;  # Temporarily disable tests
@@ -79,6 +77,12 @@
 
       in {
         packages = {
+          kernelspec = kernelspec;
+
+          install-kernel = pkgs.writeShellScriptBin "install-kernel" ''
+            ${pkgs.jupyter}/bin/jupyter kernelspec install ${kernelspec}/share/jupyter/kernels/my-data-science-kernel --user
+          '';
+
           runtimed = buildRustPackage {
             pname = "runtimed";
             cargoToml = "./runtimed/Cargo.toml";
@@ -94,7 +98,7 @@
 
         devShell = pkgs.mkShell {
           nativeBuildInputs = commonInputs;
-          buildInputs = [ pkgs.openssl pkgs.jupyter kernelPackage ];
+          buildInputs = [ pkgs.openssl pkgs.jupyter pythonEnv ];
 
           shellHook = ''
             export OPENSSL_DIR=${pkgs.openssl.dev}
@@ -111,6 +115,22 @@
 
           runt = flake-utils.lib.mkApp {
             drv = self.packages.${system}.runt;
+          };
+
+          install-kernel = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.install-kernel;
+          };
+
+          run-kernel = flake-utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "run-kernel" ''
+              if ! ${pkgs.jupyter}/bin/jupyter kernelspec list | grep -q "my-data-science-kernel"; then
+                echo "Installing kernel..."
+                ${self.packages.${system}.install-kernel}/bin/install-kernel
+              fi
+              
+              echo "Starting Jupyter console with my-data-science-kernel..."
+              ${pkgs.jupyter}/bin/jupyter console --kernel=my-data-science-kernel
+            '';
           };
         };
       }
